fc37afa25117ad75f86046746a74bf23bdac5a34
Kahvipuu <partaar@hotmail.com>
2021-02-12 12:56:38 +0200

    poiston kanssa taistelua

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import personService from './services/persons'
 
 const Filter = (props) => {
-  console.log('filter:', props.filter, 'handler', props.handleFilterChange)
+  console.log('filter:', props.nameFilter, 'handler', props.handleFilterChange)
   return (
     <div>
       <form>
         filter: <input
-          value={props.filter}
+          value={props.nameFilter}
           onChange={props.handleFilterChange}
         ></input>
       </form>
@@ -16,6 +17,7 @@ const Filter = (props) => {
 
 const SubmitForm = (props) => {
   //Varmaan tästä toivottaisiin jotain hieman paremmin jaoteltua kokonaisuutta...
+  console.log('subForm', props);
   return (
     <div>
       <form onSubmit={props.addName}>
@@ -34,24 +36,45 @@ const SubmitForm = (props) => {
 }
 
 const ShowNumbers = (props) => {
+  /* miksi täällä täytyy olla numberFilter() aaltosulkeilla,
+   mutta ylempänä SubmitFormissa ei handleissa tarvitse?? (tai ei pidä olla) --ilmeisesti event vs funktio */
+  console.log('ShowNum', props, 'ShowNum-persons', props.persons, 'ShowNum-nameFilt', props.nameFilter)
+  const numbersToFilter = props.numberFilter()
+  console.log('showNumFilter', numbersToFilter);
   return (
     <ul>
-      {props.numbersToShow().map(person => <li key={person.id}>{person.name} {person.number}</li>
-      )}
+      {numbersToFilter.map(person => {
+        return (
+          <div key={person.id}>
+            <li > {person.name} {person.number} </li>
+            <button onClick={() => props.removeName(person)} >remove </button>
+          </div>
+        )
+      })}
     </ul>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '0401231244', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    /*    { name: 'Arto Hellas', number: '0401231244', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+        */
   ])
   const [newName, setNewName] = useState('add new name')
   const [newNumber, setNewNumber] = useState('add new Number')
-  const [filter, setFilter] = useState('')
+  const [nameFilter, setFilter] = useState('')
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(response => {
+        console.log('1.datahaku',response.data);
+        setPersons(response.data)
+      })
+  }, [])
 
   const allreadyInBook = () => {
     let inBook = false
@@ -71,16 +94,34 @@ const App = () => {
     const newObject = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1,
     }
-    console.log(newName, 'includes', persons.includes(newName))
+
     if (allreadyInBook()) {
       alert(`${newName} is already in the phonebook`)
     } else {
-      setPersons(persons.concat(newObject))
-      setNewName('')
-      setNewNumber('')
+      personService
+        .create(newObject)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+        })
     }
+  }
+
+  const removeName = (person) => {
+    console.log('removeName event', person)
+    if (window.confirm(`Remove ${person.name}`)) {
+      personService
+        .remove(person.id)
+        .then(
+         
+        )
+    }
+    /*    setPersons(personService
+          .getAll()
+          .then(response => { setPersons(response.data) })
+        ) */
   }
 
   const handleNewNameChange = (event) => {
@@ -99,24 +140,24 @@ const App = () => {
     setFilter(event.target.value)
   }
 
-  const numbersToShow = () => {
-    if (filter.length === 0) {
+  const numberFilter = () => {
+    console.log('NumFilter-persons', persons, 'NumFilter-nameFilt', nameFilter)
+    if (nameFilter.length === 0) {
       return persons
     }
-
-    return persons.filter(person => person.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase()))
+    return persons.filter(person => person.name.toLocaleLowerCase().includes(nameFilter.toLocaleLowerCase()))
     // ja nyt on näköjään vaikea tajuta että funktiosta voisi jotain palauttaakin..
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+      <Filter nameFilter={nameFilter} handleFilterChange={handleFilterChange} />
       <h2>add a new number</h2>
       <SubmitForm addName={addName} newName={newName} newNumber={newNumber}
         handleNewNameChange={handleNewNameChange} handleNewNumberChange={handleNewNumberChange} />
       <h2>Numbers</h2>
-      <ShowNumbers numbersToShow={numbersToShow} />
+      <ShowNumbers removeName={removeName} persons={persons} nameFilter={nameFilter} numberFilter={numberFilter} />
     </div>
   )
 
