9c7ce169d22a71d23f28cd758961013d9352d5b4
Kahvipuu <partaar@hotmail.com>
2021-02-24 17:08:37 +0200

    ->4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+logger.info('mongoURL', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(logger.info('conn ok'))
+    .catch((err) => {
+        logger.error('error', err)
+    })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
