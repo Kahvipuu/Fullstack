c8bddf0b07d815a2d3a6ff21f4f55c4f931e7b04
Kahvipuu <partaar@hotmail.com>
2021-02-26 18:49:52 +0200

    ->4.11, !4.6-4.7

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,22 +1,37 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+
+    response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
+blogsRouter.get('/:id', async (request, response) => {
+    console.log('GET XXXXXXXXXXXXXXXXXXXXXXXXX ID', request.params.id);
+    const blog = await Blog.findById(request.params.id)
+    console.log('GET2 DDDDD', blog);
+    if (blog) {
+        response.json(blog.toJSON())
+    } else {
+        response.status(404).end()
+    }
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+})
+
+blogsRouter.post('/', async (request, response) => {
+    const body = request.body._doc
+    const likes = request.body._doc.likes === undefined ? 0 : body.likes
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: likes
+    })
+    console.log('BLOG TO SAVE hilisdsd ->', blog);
+    await blog.save()
+
+    response.json(blog.toJSON())
 })
 
 module.exports = blogsRouter
--- osa4/blogilista/tests/requests.test.js
+++ osa4/blogilista/tests/requests.test.js
@@ -24,6 +24,57 @@ test('right amount of blogs returned', async () => {
     expect(response.body).toHaveLength(helper.blogList.length)
 })
 
+test('correct form of json', async () => {
+    const response = await api.get('/api/blogs')
+    console.log('CORR FORM JSON', response.body);
+    expect(response.body[0].id).toBeDefined()
+})
+
+test('blog is added to database', async () => {
+    const newBlog = new Blog({
+        title: 'BlogAddedToDB',
+        author: 'AuthorString',
+        url: 'UrlString',
+        likes: 1
+    })
+
+    const blogsAtStart = await helper.blogsInDb()
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length + 1)
+
+})
+
+test('WIP initial default likes zero', async () => {
+    let newBlog = new Blog({
+        title: 'InitDefaulZeroLikes',
+        author: 'AuthorString',
+        url: 'UrlString'
+    })
+
+    console.log('ZEEEEEEEEEEEEEZOOOOOOOOOOOOOO', newBlog)
+
+    const postedBlog = await api
+        .post('/api/blogs/')
+        .send(newBlog)
+        .expect(200)
+
+    console.log('POSTEDBLOG XXXXXX', postedBlog.body);
+    const id = postedBlog.body.id
+    console.log('ID TO FIND LLLLLLLLL', id);
+    newBlogResponse = await api.get(`/api/blogs/${id}`)
+    console.log('ZEEEEEEEEEEEEEZUUU', newBlogResponse.body);
+
+    expect(newBlogResponse.body.likes).toBe(0)
+
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
